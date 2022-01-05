49f1af00757ac65533dd7ccec3fbf1b25441aaf2
Eman Abd El-Hamied <eman.a.hamied@gmail.com>
2022-01-05 16:53:59 +0200

    Add files via upload

---
+++ Source/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Picture_Area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            Rectangle rect = new Rectangle(5, 4);
+            Square seq = new Square(5);
+            Picture pic = new Picture(seq,rect);
+
+            Console.WriteLine("Picture Area = "+pic.Calculate_Pic_Area());
+
+            Rectangle[] rect2 = new Rectangle[2];
+            Square[] seq2 = new Square[2];
+            
+            //Squares and rectangles to array
+            rect2[0]=rect;
+            rect2[1] = rect;
+            seq2[0]=seq;
+            seq2[1] = seq;
+            //creating new picture that takes two arrays
+            Picture pic2 = new Picture(seq2, rect2);
+
+            //printing
+            Console.WriteLine("Picture Area Array " + pic2.Calculate_Array_Pic());
+        }
+    }
+}
---
+++ Source/Rectangle.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Picture_Area
+{
+    internal class Rectangle
+    {
+        int width;
+        int height;
+
+        public int Width
+        {
+            get
+            {
+                return width;
+
+            }
+            set
+            {
+                if(value < 0)
+                    width =0;
+                else
+                    width = value;
+            }
+        }
+
+        public int Height
+        {
+            get
+            {
+                return height;
+
+            }
+            set
+            {
+                if (value < 0)
+                    height = 0;
+                else
+                    height = value;
+            }
+        }
+
+       
+
+
+        public Rectangle()
+        {
+            width = 1;
+            height = 1;
+        }
+        public Rectangle(int _Width, int _Height)
+        {
+            Width = _Width;
+            Height = _Height;
+            
+        }
+
+        public int Calc_Area()
+        {
+            return height * width;
+        }
+        ~Rectangle()
+        {
+
+        }
+
+
+    }
+}
